86e13e7a9741bba21566c165e3c38eec9dd8e1ea
Hemraj Chauhan <hemraj1712@gmail.com>
2025-05-23 12:15:53 +0200

    Added message on flyio webpage

--- app/main.py
+++ app/main.py
@@ -4,3 +4,8 @@ from app.routers import grammar, translate
 app = FastAPI()
 app.include_router(grammar.router)
 app.include_router(translate.router)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to GermanAI-backend! Visit /docs for API documentation."}
+
